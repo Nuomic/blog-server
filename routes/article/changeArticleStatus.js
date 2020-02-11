975aa37ab0df8fb9bf41aca95f7877c5d920cc42
zhangweiqiang666 <972618478@qq.com>
2020-02-11 20:27:00 +0800

    修改逻辑 当文章状态为3时 同时修改文章的栏目id

--- routes/article/changeArticleStatus.js
+++ routes/article/changeArticleStatus.js
@@ -1,11 +1,17 @@
 const { resTemp, errTemp } = require('../config');
 const { ArticleModel } = require('../../db');
-
+const { Types } = require('mongoose');
 module.exports = (req, res) => {
   console.log('req.body', req.body);
   const { articleId, status } = req.body;
   if (!!articleId) {
-    ArticleModel.findByIdAndUpdate(articleId, { status }, (err, article) => {
+    let value = { status };
+    if (status == 3)
+      value = {
+        ...value,
+        categoryId: '5e3d00cf765d392e685654b3'
+      };
+    ArticleModel.findByIdAndUpdate(articleId, value, (err, article) => {
       if (!!err) return res.json(errTemp(err, ''));
       (!!article && res.json(resTemp({ article }))) ||
         res.json(errTemp(err, ''));
