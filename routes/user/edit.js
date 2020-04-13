46247c067554a2218bc1d3941e7999c7f5c81c49
zhangweiqiang666 <972618478@qq.com>
2020-04-13 11:58:19 +0800

    修改密码

--- routes/user/edit.js
+++ routes/user/edit.js
@@ -3,19 +3,30 @@ const { resTemp, errTemp } = require('../config');
 const filter = { password: 0, _v: 0 };
 module.exports = (req, res) => {
   const { userId, oldPassword, newPassword } = req.body;
-  const compare = (reqPassword, dbPassword) => {
-    return require('bcryptjs').compareSync(reqPassword, dbPassword);
+  const changePwd = (value) => {
+    UserModel.findByIdAndUpdate(userId, value, (err, userInfo) => {
+      console.log('userInfo', userInfo);
+      if (err) return res.json(errTemp(err, '数据库错误'));
+      if (userInfo) {
+        res.json(resTemp({ msg: '修改成功' }));
+      } else {
+        res.json(errTemp(err, '修改密码失败，请稍后再试！'));
+      }
+    });
   };
-  const dbPassword = UserModel.findById(userId, (err, userInfo) => {
-    if (err) return res.json(errTemp(err, ''));
+  UserModel.findById(userId, (err, userInfo) => {
+    if (err) return res.json(errTemp(err, '数据库错误'));
     if (userInfo) {
-      return userInfo.password;
+      const isPasswordValid = require('bcryptjs').compareSync(
+        oldPassword,
+        userInfo.password
+      );
+      if (newPassword) {
+        if (isPasswordValid) changePwd({ password: newPassword });
+        else res.json(errTemp(err, '原密码错误，修改失败'));
+      } else res.json(errTemp(err, '请输入新密码'));
+    } else {
+      res.json(errTemp(err, '用户不存在'));
     }
   });
-  // const getDbPassword = async () => {
-  //   return await dbPassword;
-  // };
-  // console.log('object', getDbPassword());
-  if (oldPassword) {
-  }
 };
